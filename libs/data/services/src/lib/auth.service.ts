f99978010d0e4932f25985a3ab19a0a48b342680
Joel Nieto <joelnietotec@outlook.com>
2022-05-30 15:31:31 -0500

    fix

--- libs/data/services/src/lib/auth.service.ts
+++ libs/data/services/src/lib/auth.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { APP_CONFIG } from '@tips/app-config';
 import { Login, User } from '@tips/data/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    @Inject(APP_CONFIG) private config: any
+  ) {}
 
   public login = (login: Login) =>
-    this.http.post<{ access_token: string }>(`/api/auth/login`, login);
+    this.http.post<{ access_token: string }>(
+      `${this.config.backendURL}/auth/login`,
+      login
+    );
 
   public getProfile = () => this.http.get<User>('/api/auth/profile');
 }
