fe149b732e7866f068dfef488f79aeee201296bc
Joel Nieto <joelnietotec@outlook.com>
2022-05-29 22:17:09 -0500

    fix

--- apps/api/src/app/auth/strategies/jwt.strategy.ts
+++ apps/api/src/app/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,10 @@
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { pick } from 'lodash';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     // TODO: Add cookie auth
