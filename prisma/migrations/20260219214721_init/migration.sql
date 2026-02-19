15b7dc1530b10826bbb82f718ddf28bf8c61c264
Joel Nieto <joelnieto1215@gmail.com>
2026-02-19 16:48:45 -0500

    Update .gitignore, package.json, and bun.lock to include Prisma and dotenv dependencies. Added generated/prisma and .env to .gitignore. Updated @types/node and added @types/pg.

---
+++ prisma/migrations/20260219214721_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
