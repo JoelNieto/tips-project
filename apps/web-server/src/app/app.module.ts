e4d9c22da2f9b4313ba5b5ac9038d8512641141d
Joel Nieto <joelnieto1215@gmail.com>
2026-02-19 17:07:24 -0500

    Enhance GraphQL integration by adding Apollo Server and related dependencies. Updated package.json and bun.lock to include @apollo/server, @nestjs/apollo, and GraphQL. Configured GraphQLModule in app.module.ts for schema generation and Apollo landing page plugin.

--- apps/web-server/src/app/app.module.ts
+++ apps/web-server/src/app/app.module.ts
@@ -1,9 +1,29 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        autoSchemaFile: join(process.cwd(), 'schema.gql'),
+        sortSchema: true,
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        path: '/api/graphql',
+        context: ({ req, res }) => ({ req, res }),
+      }),
+      driver: ApolloDriver,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
